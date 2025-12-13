06b74d414ed36a7243402c5c31d87a728e6bec32
emmaknisbacher <emmaknisbacher@gmail.com>
2025-12-13 16:08:01 +0000

    created index.html as main page

--- src/index.ts
+++ src/index.ts
@@ -9,27 +9,8 @@ const app = express()
 
 // Home route - HTML
 app.get('/', (req, res) => {
-  res.type('html').send(`
-    <!doctype html>
-    <html>
-      <head>
-        <meta charset="utf-8"/>
-        <title>Express on Vercel</title>
-        <link rel="stylesheet" href="/style.css" />
-      </head>
-      <body>
-        <nav>
-          <a href="/">Homer!!</a>
-          <a href="/about">About</a>
-          <a href="/api-data">API Data</a>
-          <a href="/healthz">Health</a>
-        </nav>
-        <h1>Welcome to Express on Vercel 🚀</h1>
-        <p>This is a minimal example without a database or forms.</p>
-        <img src="/logo.png" alt="Logo" width="120" />
-      </body>
-    </html>
-  `)
+  // res.type('html').send(``)
+  res.sendFile(path.join(__dirname, '..', 'components', 'index.html'))
 })
 
 app.get('/about', function (req, res) {
@@ -52,4 +33,4 @@ app.get('/healthz', (req, res) => {
 export default app
 
 
-//svo.bz/123 (or abc)
+// svo.bz/abc (or 123)
