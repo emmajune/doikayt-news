c8c3376e7a595acbde55cfd26fd40456af341d85
emmaknisbacher <emmaknisbacher@gmail.com>
2026-02-13 23:24:49 -0500

    cors workaround take 2

--- src/cache/neoCache.js
+++ src/cache/neoCache.js
@@ -1,13 +1,14 @@
 import NeoCities from 'neocities'
 import {writeFile} from 'fs/promises'
+import cssEscape from 'css.escape'
 
 export async function neoCache(jsonStr) {
-  
-  await writeFile('/tmp/cache.json', jsonStr)
+  const escJson = cssEscape(jsonStr)
+  await writeFile('/tmp/cache.css', `data { content: "${escJson}" }`)
   
   var api = new NeoCities('svrss', 'B?3ny8aZ9Q~M"tZ')
   api.upload([
-    {name: 'cache.json', path: '/tmp/cache.json'}
+    {name: 'cache.css', path: '/tmp/cache.css'}
   ], function(resp) {
     return resp
   })
