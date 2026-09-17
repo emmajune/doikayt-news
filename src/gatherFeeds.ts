e94938ef8720b050ab30ffcef7a7cc8638f2307f
Emma Knisbacher <emmaknisbacher@gmail.com>
2026-09-17 11:24:59 -0400

    new source

--- src/gatherFeeds.ts
+++ src/gatherFeeds.ts
@@ -204,6 +204,7 @@ export async function gatherFeeds() {
     leftycartoons: {url: 'https://leftycartoons.com/rss', origin: 'US'},
     unicornriot: {url: "https://www.unicornriot.ninja/rss", origin: "US"},
     leftvoice: {url: 'https://www.leftvoice.org/rss', origin: 'International'},
+    newhumanitarian: {url: 'https://www.thenewhumanitarian.org/feed', origin: 'International'}
     // hongkongfreepress: {'url': 'https://hongkongfp.com/rss', origin: 'China'} not explicitly social-justice focused    // newlefttimes -- monthlyyy
 
     
