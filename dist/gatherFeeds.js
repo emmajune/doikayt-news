eca15da30951c330435c8a41773c43910e05fb19
Emma Knisbacher <emmaknisbacher@gmail.com>
2026-09-19 20:04:59 -0400

    new build, idk

--- dist/gatherFeeds.js
+++ dist/gatherFeeds.js
@@ -200,7 +200,8 @@ export async function gatherFeeds() {
         leftvoice: { url: 'https://www.leftvoice.org/rss', origin: 'International' },
         africaisacountry: { 'url': 'https://africasacountry.com/feed', origin: 'International' },
         icij: { 'url': 'https://www.icij.org/rss', origin: 'International' },
-        dailycartoon: { 'url': 'https://cmdailycartoon.substack.com/rss', origin: 'International' }
+        dailycartoon: { 'url': 'https://cmdailycartoon.substack.com/rss', origin: 'International' },
+        breakthrough: { 'url': 'https://breakthroughnews.org/rss', origin: 'International' }
     };
     var t1 = performance.now();
     await fetchRawFeeds(sourcesObj);
--- dist/index.js
+++ dist/index.js
@@ -54,7 +54,7 @@ async function sleep(ms) {
 async function reCache() {
     cachedJson = await gatherFeeds();
     await neoCache(cachedJson);
-    await sleep(60000);
+    await sleep(5000);
     reCache();
     return undefined;
 }
